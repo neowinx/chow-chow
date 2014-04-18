cd9bc03ad84c4f19d3d143fd2ffeef8f415d990f
Pedro Flores <pflores@dncp.gov.py>
2014-04-18 17:23:00 -0400

    Added some samples for reference and cutting some edges

--- chow-chow.py
+++ chow-chow.py
@@ -3,8 +3,9 @@
 
 import wx
 
-TRAY_TOOLTIP = 'System Tray Demo'
+TRAY_TOOLTIP = 'Chow Chow'
 TRAY_ICON = 'drawing.png'
+FRAME_ICON = 'icon32.png'
 
 
 def create_menu_item(menu, label, func):
@@ -14,10 +15,11 @@ def create_menu_item(menu, label, func):
     return item
 
 
-class MyTaskBarIcon(wx.TaskBarIcon):
+class ChowChowTaskBarIcon(wx.TaskBarIcon):
 
-    def __init__(self):
-        super(MyTaskBarIcon, self).__init__()
+    def __init__(self, frame):
+        super(ChowChowTaskBarIcon, self).__init__()
+        self.frame = frame
         self.set_icon(TRAY_ICON)
         self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
 
@@ -33,23 +35,29 @@ class MyTaskBarIcon(wx.TaskBarIcon):
         self.SetIcon(icon, TRAY_TOOLTIP)
 
     def on_left_down(self, event):
-        print 'Tray icon was left-clicked.'
+        if self.frame.IsShown():
+            self.frame.Show(False)
+        else:
+            self.frame.Show(True)
 
     def on_hello(self, event):
         print 'Hello, world!'
 
     def on_exit(self, event):
         wx.CallAfter(self.Destroy)
+        wx.CallAfter(self.frame.Destroy)
 
 
-class Example(wx.Frame):
-    def __init__(self, parent, title):
-        super(Example, self).__init__(parent, title=title,size=(250, 200))
-        self.Show()
+class ChowChowFrame(wx.Frame):
+
+    def __init__(self, parent, id, title):
+        wx.Frame.__init__(self, parent, id, title, size=(250, 200))
+        self.tbicon = ChowChowTaskBarIcon(self)
+        self.SetIcon(wx.IconFromBitmap(wx.Bitmap(FRAME_ICON)))
+        self.Show(False)
 
 
 if __name__ == '__main__':
     app = wx.App()
-    Example(None, title='Size')
-    MyTaskBarIcon()
+    frame = ChowChowFrame(None, -1, 'Chow Chow 0.0.1-SNAPSHOT')
     app.MainLoop()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='chow-chow',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='neowinx',
+    author_email='',
+    description=''
+)
