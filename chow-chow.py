1ef78516b8d3d2c23fc4e01e95c15e09e288d3c8
Pedro Flores <pflores@dncp.gov.py>
2014-04-21 01:05:47 -0400

    Added some wxFormBuilder stuff and also starting to divide the proyect in little neat packages :)

--- chow-chow.py
+++ chow-chow.py
@@ -3,53 +3,12 @@
 
 import wx
 
+from gui.extra import PromptingComboBox, create_menu_item
+
 TRAY_TOOLTIP = 'Chow Chow'
 TRAY_ICON = 'icon32.png'
 FRAME_ICON = 'icon32.png'
 DEFAULT_TASKS = ['Breakfast', 'Lunch']
-
-
-def create_menu_item(menu, label, func):
-    item = wx.MenuItem(menu, -1, label)
-    menu.Bind(wx.EVT_MENU, func, id=item.GetId())
-    menu.AppendItem(item)
-    return item
-
-
-class PromptingComboBox(wx.ComboBox):
-    def __init__(self, parent, value, choices=[], style=0, **par):
-        wx.ComboBox.__init__(self, parent, wx.ID_ANY, value, style=style | wx.CB_DROPDOWN, choices=choices, **par)
-        self.choices = choices
-        self.Bind(wx.EVT_TEXT, self.EvtText)
-        self.Bind(wx.EVT_CHAR, self.EvtChar)
-        self.Bind(wx.EVT_COMBOBOX, self.EvtCombobox)
-        self.ignoreEvtText = False
-
-    def EvtCombobox(self, event):
-        self.ignoreEvtText = True
-        event.Skip()
-
-    def EvtChar(self, event):
-        if event.GetKeyCode() == 8:
-            self.ignoreEvtText = True
-        event.Skip()
-
-    def EvtText(self, event):
-        if self.ignoreEvtText:
-            self.ignoreEvtText = False
-            return
-        currentText = event.GetString()
-        found = False
-        for choice in self.choices :
-            if choice.startswith(currentText):
-                self.ignoreEvtText = True
-                self.SetValue(choice)
-                self.SetInsertionPoint(len(currentText))
-                self.SetMark(len(currentText), len(choice))
-                found = True
-                break
-        if not found:
-            event.Skip()
 
 
 class ChowChowTaskBarIcon(wx.TaskBarIcon):
@@ -81,34 +40,43 @@ class ChowChowTaskBarIcon(wx.TaskBarIcon):
 class ChowChowFrame(wx.Frame):
 
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title)
+        wx.Frame.__init__(self, parent, id, title, size=wx.Size(-1, -1))
         self.task_bar_icon = ChowChowTaskBarIcon(self)
         self.SetIcon(wx.IconFromBitmap(wx.Bitmap(FRAME_ICON)))
         self.Show(True)
 
         self.Bind(wx.EVT_CLOSE, self.on_close)
 
-        panel = wx.Panel(self)
-        sizer = wx.GridBagSizer(4, 4)
+        panel = wx.Panel(self, size=wx.DefaultSize)
+        gb_sizer = wx.GridBagSizer(0, 0)
+        gb_sizer.SetFlexibleDirection(wx.BOTH)
+        gb_sizer.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
         lbl_insert_task = wx.StaticText(panel, label="Insert your task:")
-        sizer.Add(lbl_insert_task, pos=(0, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=5)
+        gb_sizer.Add(lbl_insert_task, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.ALL, 5)
 
         self.cmb_task = PromptingComboBox(panel, '', DEFAULT_TASKS, style=wx.CB_SORT)
         self.cmb_task.Bind(wx.EVT_KEY_DOWN, self.key_down)
         self.cmb_task.Bind(wx.EVT_KEY_UP, self.key_up)
-        sizer.Add(self.cmb_task, pos=(1, 0), span=(1, 5), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
+        gb_sizer.Add(self.cmb_task, pos=(1, 0), span=(1, 1), flag=wx.EXPAND | wx.LEFT | wx.RIGHT, border=5)
 
         button_ok = wx.Button(panel, label="Ok", size=(90, 28))
         button_close = wx.Button(panel, label="Close", size=(90, 28))
-        sizer.Add(button_ok, pos=(3, 3))
-        sizer.Add(button_close, pos=(3, 4), flag=wx.RIGHT | wx.BOTTOM, border=5)
 
-        sizer.AddGrowableCol(1)
-        sizer.AddGrowableRow(2)
+        gb_sizer.Add(button_ok, pos=(3, 3))
+        gb_sizer.Add(button_close, pos=(3, 4), flag=wx.RIGHT | wx.BOTTOM, border=5)
 
-        panel.SetSizerAndFit(sizer)
-        self.Fit()
+        panel_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        panel_sizer.Add(gb_sizer)
+
+        panel.SetSizerAndFit(panel_sizer)
+
+        main_bsizer = wx.BoxSizer(wx.VERTICAL)
+
+        main_bsizer.Add(panel)
+
+        self.SetSizer(main_bsizer)
         self.Centre()
         self.cmb_task.SetFocus()
 
