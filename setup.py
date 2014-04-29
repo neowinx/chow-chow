d70285c9189017e456e3f01d05771e0cf12fa9f1
Pedro Flores <pflores@dncp.gov.py>
2014-04-29 16:57:40 -0400

    Added a basic (probably not working) setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='chow-chow',
+    version='0.0.1',
+    packages=['gui', 'code'],
+    url='http://github.com/neowinx',
+    license='LGPL',
+    author='neowinx',
+    author_email='pflores@codelab.com.py',
+    description='A simple tool (or at least tha\'ts what It claims) to track time spent on tasks '
+)
